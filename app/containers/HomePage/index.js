4bc3265b51c7c6df6ad0986de66cb2337fe906f4
Mark McQuillen <markdmcq@hotmail.com>
2017-01-31 13:16:41 -0800

    Simple form is console.logging that a form has been submitted

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -15,9 +15,14 @@ import messages from './messages';
 
 import Form from './Form';
 import Input from './Input';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 
-export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+
+
+export class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+
   render() {
     return (
       <div>
@@ -37,3 +42,21 @@ export default class HomePage extends React.PureComponent { // eslint-disable-li
     );
   }
 }
+
+
+export function mapDispatchToProps(dispatch) {
+  return {
+    onSubmitForm: (evt) => {
+      if (evt !== undefined && evt.preventDefault) evt.preventDefault();
+      console.log("You submitted a form!!!!");
+      // dispatch(loadRepos());
+    },
+  };
+}
+
+const mapStateToProps = createStructuredSelector({
+});
+
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
